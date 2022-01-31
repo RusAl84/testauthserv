2b65d5c879ac79ec419bc161433dae1c5f6580d9
Aleksey <rusal@bk.ru>
2022-01-31 03:40:14 +0300

    add messenger

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ ListOfColors=['maroon',
     'orange',
     'aquamarine']	
 
-
-SecretKey=['red','blue','green','black']
+SecretKey=['blue','green', 'red']
 currentKey=[]
 
+ListOfMessages = []
 
 @app.route('/')
 def dafault_route():
@@ -50,7 +50,7 @@ def sendColor():
 
 # получение цветов
 @app.route("/color/<int:id>")
-def GetMessage(id):
+def GetColor(id):
     print(id)
     if id >= 0 and id < len(ListOfColors):
         print(ListOfColors[id])
@@ -65,6 +65,37 @@ def status():
     for color in ListOfColors:
         allcolors += "<br> " + color
     return allcolors
+    
+# отправка сообщений
+@app.route("/mes", methods=['POST'])
+def SendMessage():
+    msg=""
+    msg = request.json
+    print(msg)
+    # messages.append({ "UserName":"RusAl","MessageText":"Privet na sto let!!!","TimeStamp":"2021-03-05T18:23:10.932973Z"})
+    ListOfMessages.append(msg)
+    print(msg)
+    msgtext = f"{msg['UserName']} <{msg['TimeStamp']}>: {msg['MessageText']}"
+    print(f"Всего сообщений: {len(ListOfMessages)} Посланное сообщение: {msgtext}")
+    return f"Сообщение отослано успшно. Всего сообщений: {len(ListOfMessages)} ", 200
+
+# получение сообщений
+@app.route("/mes/<int:id>")
+def GetMessage(id):
+    print(id)
+    if id >= 0 and id < len(ListOfMessages):
+        print(ListOfMessages[id])
+        return ListOfMessages[id], 200
+    else:
+        return "Not found", 400
+
+@app.route('/statusMes')
+def statusMes():
+    allmessages = ""
+    for msg in ListOfMessages:
+        allmessages += "<br> " + f"{msg['UserName']} <{msg['TimeStamp']}>: {msg['MessageText']}"
+    allmessages +=f'<br> Count of Messages {len(ListOfMessages)}'
+    return allmessages
 
 if __name__ == '__main__':
     random.shuffle(ListOfColors)
